eef38e747752c8fe56f02e40868c087a057e4d8e
woshi <159062110+woshimarcel@users.noreply.github.com>
2025-10-09 22:00:42 +0200

    Added categories & selection

--- Logic/SaveKeys.cs
+++ Logic/SaveKeys.cs
@@ -5,4 +5,5 @@ public class SaveKeys
 	public const string MOST_COMMON_GHOSTS = "mostCommonGhosts";
 	public const string PLAYED_MAPS = "playedMaps";
 	public const string GHOST_KILLS = "ghostKills";
+	public const string BONE = "Bone"; // + index number
 }
