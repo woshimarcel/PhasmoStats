d8fbbe4935ce47c20bfe8fa574dc620dbb87f3be
woshi <159062110+woshimarcel@users.noreply.github.com>
2025-11-15 14:18:01 +0100

    Added sorting indicators for sortable columns

---
+++ Logic/SortIndicator.cs
@@ -0,0 +1,19 @@
+﻿namespace Logic;
+
+public class SortIndicator
+{
+	public static string GetHeaderName(string header, string sortingColumn, bool ascending)
+	{
+		const char ASCENDING_CHARACTER = '⬆';
+		const char DESCENDING_CHARACTER = '⬇';
+		const char NOTHING = '▪';
+
+		if (header != sortingColumn)
+			return $"{header} {NOTHING}";
+
+		if (ascending)
+			return $"{header} {ASCENDING_CHARACTER}";
+		else
+			return $"{header} {DESCENDING_CHARACTER}";
+	}
+}
